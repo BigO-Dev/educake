fcda6e8e4471c22d3abd4153895b9069ce46849a
Omair Majid <omajid.dev@gmail.com>
2024-06-24 15:18:41 -0700

    chore: Update react-icons to version 5.2.1 and add SidebarMenu component

--- client/src/components/SidebarMenu.jsx
+++ client/src/components/SidebarMenu.jsx
@@ -1,8 +1,14 @@
 import { SiGoogleclassroom } from 'react-icons/si'
 import { LiaSchoolSolid } from 'react-icons/lia'
+import { TbLayoutSidebarLeftCollapse } from 'react-icons/tb'
 
 const SidebarMenu = () => {
-  return <div className='drawer lg:drawer-open'></div>
+  return (
+    <div className='drawer lg:drawer-open '>
+      <input id='my-drawer' type='checkbox' className='drawer-toggle' />
+      <div className='drawer-content'>{/* content */}</div>
+    </div>
+  )
 }
 
 export default SidebarMenu
