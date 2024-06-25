54a4db5dd2f3bd855eda8bd18f710c0ded33d34e
Omair Majid <omajid.dev@gmail.com>
2024-06-25 15:19:18 -0700

    chore: Remove unused files and dependencies

--- client/App.jsx
+++
@@ -1,5 +0,0 @@
-import SidebarMenu from './components/SidebarMenu'
-
-export default function App() {
-  return <SidebarMenu />
-}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-import './css/style.css';
-import './css/satoshi.css';
-import 'jsvectormap/dist/css/jsvectormap.css';
-import 'flatpickr/dist/flatpickr.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
-);
+)
